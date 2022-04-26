147b7c6a074e026c548eabb8a769b47132e08f96
lucas bentes <lucasbentes16@gmail.com>
2022-04-26 13:10:08 -0300

    atualizacoes

---
+++ assets/script/script.js
@@ -0,0 +1,54 @@
+let barraProgreso = document.querySelector(".barraProgreso");
+
+let nome = document.querySelector(".nomeValidar");
+let email = document.querySelector(".emailValidar");
+let mensagem = document.querySelector(".mensagemValidar");
+let textMensagem = document.querySelector(".textMensagem");
+let contador = 0;
+let nomeOk = false;
+let emailOk = false;
+let mensagemOk = false;
+
+textMensagem.innerHTML = "Numero de caracter " + contador + "/100";
+
+function validarNome(){
+    if(nome.value.length < 3){
+        nome.classList.add("is-invalid");
+    }else{
+        nome.classList.remove("is-invalid");
+        nome.classList.add("is-valid");
+        nomeOk = true;
+    }
+}
+
+function validarEmail(){
+    if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1){
+        email.classList.add("is-invalid");
+    }else{
+        email.classList.remove("is-invalid");
+        email.classList.add("is-valid");
+        emailOk = true;
+    }
+}
+
+function validarMensagem(){
+    contador = mensagem.value.length;
+    textMensagem.innerHTML = "Numero de caracter " + contador + "/100";
+
+    if (assunto.value.length >= 100) {
+        textMensagem.style.color = 'red !important';
+        mensagem.classList.add("is-invalid");
+     } else {
+        textMensagem.style.color = "ligth";
+        mensagem.classList.remove("is-invalid");
+        mensagemOk = true
+     }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && mensagemOk == true) {
+       alert ('Formulário enviado com sucesso!');
+    } else {
+       alert ('Preencha o formulário corretamente antes de enviar...');
+    }
+}
